3fb1f918a0157ba5d160c54723ecacf61f9b544f
hugo <hugof16silva@gmail.com>
2023-08-22 09:24:30 -0300

    feat: Adicionado react-router-dom@6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 export const App = () => {
   return (
-    <div className="App">
-      teste
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
